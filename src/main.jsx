0edc14662c0c593b6490da8f0d0885a83047f0bd
khachikkar <khachikkarapetyan78@gmail.com>
2025-03-24 10:50:48 +0400

    m

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import 'antd/dist/reset.css';
+// import 'antd/dist/reset.css';
 
 
 import { Toaster } from "react-hot-toast";
